c79178ff511f7df1fa576959cf15c0cc6ba5100d
Saurav Khare <saurav.erahk@gmail.com>
2024-12-03 00:26:15 +0530

    Fixed build error

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,10 +4,6 @@ import type { Metadata } from "next";
 import { GeistSans } from "geist/font/sans";
 import { GeistMono } from "geist/font/mono";
 import QueryProvider from "../context/query-context";
-import TickerBar from "@/components/TickerBar";
-import Header from "@/components/Header";
-import Footer from "@/components/Footer";
-import BackgroundGradient from "@/components/BackgroundGradient";
 
 export const metadata: Metadata = {
 	title: "Saurav Khare",
@@ -23,7 +19,6 @@ export default function RootLayout({
 	return (
 		<html lang="en" className={`${GeistSans.variable} ${GeistMono.variable}`}>
 			<body className="bg-[#111111] bg-[radial-gradient(circle,#222,#111)]">
-				{/* <TickerBar /> */}
 				<div className="text-white mx-auto md:max-w-screen-xl px-8 lg:px-24">
 					<QueryProvider>{children}</QueryProvider>
 				</div>
