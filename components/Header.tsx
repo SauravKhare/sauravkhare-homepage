44c661f0f86a89170f0a180224365b2c78342c56
Saurav Khare <saurav.erahk@gmail.com>
2024-12-03 01:25:26 +0530

    Minor changes

--- components/Header.tsx
+++ components/Header.tsx
@@ -6,37 +6,31 @@ import Socials from "./Socials";
 export default function Header() {
 	return (
 		<>
-			<header className="">
-				<div className="max-w-[50rem] md:mx-auto  flex justify-between items-center">
-					<div className="">
-						<Link href="/">
-							<div className="">
-								<Heading
-									classname="font-OPTI text-4xl text-red-700 mb-3"
-									headingType="h1"
-								>
-									Saurav Khare
-								</Heading>
-							</div>
-						</Link>
-						<Paragraph classname="text-base font-OPTI text-white mb-3">
-							Frontend Engineer.
-						</Paragraph>
-					</div>
-				</div>
-				<Paragraph classname="text-lg mb-4 font-inter text-white/60">
-					I&apos;m an experienced frontend developer with a specialization in
-					React and its broad ecosystem. Possessing a sharp eye for design and a
-					talent for creating smooth user experiences, I excel at bringing
-					concepts to life.
+			<header className="mb-6 md:mb-16">
+				<Link href="/">
+					<Heading
+						classname="font-OPTI text-4xl text-red-700 mb-3"
+						headingType="h1"
+					>
+						Saurav Khare
+					</Heading>
+				</Link>
+				<Paragraph classname="text-base font-OPTI text-white mb-3">
+					Frontend Engineer
 				</Paragraph>
-				<Paragraph classname="text-lg mb-4 font-inter text-white/60">
-					Beyond my professional interests, I&apos;m a passionate movie buff.
-					Discussing and dissecting films is a real joy for me, especially when
-					it comes to exploring the artistry of visual storytelling.
-				</Paragraph>
-				<Socials />
 			</header>
+			<Paragraph classname="text-lg mb-4 font-inter text-white/60">
+				I&apos;m an experienced frontend developer with a specialization in
+				React and its broad ecosystem. Possessing a sharp eye for design and a
+				talent for creating smooth user experiences, I excel at bringing
+				concepts to life.
+			</Paragraph>
+			<Paragraph classname="text-lg mb-4 font-inter text-white/60">
+				Beyond my professional interests, I&apos;m a passionate movie buff.
+				Discussing and dissecting films is a real joy for me, especially when it
+				comes to exploring the artistry of visual storytelling.
+			</Paragraph>
+			<Socials />
 		</>
 	);
 }
--- components/Socials.tsx
+++ components/Socials.tsx
@@ -1,37 +1,15 @@
-"use client";
-
-import { useState } from "react";
-import Paragraph from "./Paragraph";
 import { Connect } from "@/constants";
 import Link from "next/link";
 import Image from "next/image";
 
 export default function Socials() {
-	const [emailVisible, setEmailVisible] = useState(false);
-
 	return (
 		<div className="flex gap-5 align-middle flex-wrap">
 			{Connect.map((item) => (
-				<Link key={item.name} href={item.value}>
-					<Image
-						src={item.image}
-						width={28}
-						height={28}
-						alt={item.name}
-						className="hover:bg-black"
-					/>
+				<Link key={item.name} href={item.value} target="_blank">
+					<Image src={item.image} width={28} height={28} alt={item.name} />
 				</Link>
 			))}
-			<Paragraph classname="mb-3 text-base font-normal font-geist-sans text-gray-500">
-				<span
-					onClick={() => {
-						setEmailVisible((prev) => !prev);
-					}}
-					className={`cursor-pointer ${emailVisible ? "blur-none" : "blur-sm"}`}
-				>
-					{emailVisible ? "saurav.erahk@gmail.com" : "No bots please!"}
-				</span>
-			</Paragraph>
 		</div>
 	);
 }
