76c1ef4af542b00faf5e6274fc8f8a1e171e2cbb
Saurav Khare <saurav.erahk@gmail.com>
2025-07-16 18:17:58 +0530

    chore: created migrations

---
+++ migrations/index.ts
@@ -0,0 +1,9 @@
+import * as migration_20250716_124545 from './20250716_124545';
+
+export const migrations = [
+  {
+    up: migration_20250716_124545.up,
+    down: migration_20250716_124545.down,
+    name: '20250716_124545'
+  },
+];
