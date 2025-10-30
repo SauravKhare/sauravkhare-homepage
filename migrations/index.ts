de9cc4e8d19a8077128d557b882288a711b1d242
Saurav Khare <saurav.erahk@gmail.com>
2025-10-31 00:27:07 +0530

    fix: fixed migration issues

--- migrations/index.ts
+++
@@ -1,9 +0,0 @@
-import * as migration_20251030_184916 from './20251030_184916';
-
-export const migrations = [
-  {
-    up: migration_20251030_184916.up,
-    down: migration_20251030_184916.down,
-    name: '20251030_184916'
-  },
-];
