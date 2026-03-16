90ef6f2c1a7bbaf19bedffc1667926443bf328ec
Saurav Khare <saurav.erahk@gmail.com>
2026-03-16 17:47:01 +0530

    chore: migrations

--- migrations/index.ts
+++ migrations/index.ts
@@ -4,6 +4,7 @@ import * as migration_20251205_231953 from './20251205_231953';
 import * as migration_20260215_205617 from './20260215_205617';
 import * as migration_20260304_214511 from './20260304_214511';
 import * as migration_20260316_112606 from './20260316_112606';
+import * as migration_20260316_121639 from './20260316_121639';
 
 export const migrations = [
   {
@@ -34,6 +35,11 @@ export const migrations = [
   {
     up: migration_20260316_112606.up,
     down: migration_20260316_112606.down,
-    name: '20260316_112606'
+    name: '20260316_112606',
+  },
+  {
+    up: migration_20260316_121639.up,
+    down: migration_20260316_121639.down,
+    name: '20260316_121639'
   },
 ];
